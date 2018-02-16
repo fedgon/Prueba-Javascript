8893e23f4a02f0a8179f1705048b7c1af268dae1
Federico González <federico.gonzalez@mail.udp.cl>
2018-02-16 10:37:50 -0300

    Base html armada con AJAX

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $.ajax({
+    url: 'https://api.qwant.com/api/search/images?count=20&offset=1&q=forest',
+    method: "GET"
+    }).done(function() {
+      console.log("success");
+    })
+
+    data.media
+
+
+
+
+
+});
