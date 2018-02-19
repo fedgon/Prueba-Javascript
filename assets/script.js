1aadf79ab81389b200d439daf4e96ab17d27ac98
Federico González <federico.gonzalez@mail.udp.cl>
2018-02-19 10:18:30 -0300

    html y css armados, js por finalizar

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
 
   $.ajax({
-    url: 'https://api.qwant.com/api/search/images?count=20&offset=1&q=forest',
+    url:'https://cors-anywhere.herokuapp.com/https://api.qwant.com/api/search/images?count=20&offset=1&q=forest',
     method: "GET"
-    }).done(function() {
-      console.log("success");
+  }).done(function(data) {
 
-      photo = data.result.items.first().media
+    console.log(data)
 
-      $('.main-picture').attr('src', photo)
+    // $('.picture-1').attr('src', data.result.items[0].media);
+
 
     })
 
-  $('.gallery img').click(function(e){
-    e.preventDefault()
+    $('.gallery img').click(function(e){
+      e.preventDefault()
 
-    var photoSelect = $(this).attr('src');
-    $('.main-picture').attr('src', photoSelect);
-  })
+      var photoSelect = $(this).attr('src');
+      $('.main-picture').attr('src', photoSelect);
+   })
+
+
+
+
+
+  // $('.picture').hover(function(){
+  //
+  //   var imageSelect = $(this).parent()
+  //
+  //   imageSelect.append('<h6>AUTO 1</h6><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>')
+  //
+  // })
 
 });
