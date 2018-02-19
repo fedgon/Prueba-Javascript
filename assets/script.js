8900d4913af19aaaae735af68c4229860cd88676
Federico González <federico.gonzalez@mail.udp.cl>
2018-02-19 19:00:39 -0300

    TERMINADO

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,44 @@ $(document).ready(function() {
 
   $.ajax({
     url:'https://cors-anywhere.herokuapp.com/https://api.qwant.com/api/search/images?count=20&offset=1&q=forest',
-    method: "GET"
-  }).done(function(data) {
+    method: 'GET',
+    dataType: 'json',
+      success: function(response) {
 
-    console.log(data)
+        console.log(response);
 
+        var toHtml = "";
+
+        response.data.result.items.forEach(function(itemFoto) {
+
+          toHtml += '<div class="image-box">'+
+          '<div class="picture" style="background-image: url('+ itemFoto.media +');background-size: cover;"></div>'+
+          '<div class="wrapper hidden">'+
+          '<h3 class="imag-name">'+ itemFoto.title +'</h3>'+
+          '<p class="imag-size">'+ itemFoto.width + 'x' + itemFoto.height +'</p>'+
+          '<a class="imag-url" href=""' + itemFoto.url + '">url</a>'+
+          '</div>'+
+          '</div>'
+
+        })
+
+        $('.gallery').html(toHtml)
+
+    $('')
+  }
+  })
     // $('.picture-1').attr('src', data.result.items[0].media);
 
-
-    })
-
-    $('.gallery img').click(function(e){
+    $('.gallery').on('click', '.image-box .picture', function(e){
       e.preventDefault()
 
-      var photoSelect = $(this).attr('src');
-      $('.main-picture').attr('src', photoSelect);
+      var photoSelect = $(this).css('background-image');
+      var anotherPhoto = photoSelect.split('"');
+      // console.log(anotherPhoto[1])
+      // <img class="main-picture" src="">
+
+      $('.container').html('<img class="main-picture" src='+ anotherPhoto[1] + '>')
    })
-
-
-
-
 
   // $('.picture').hover(function(){
   //
